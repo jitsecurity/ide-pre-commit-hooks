755f647e925faee535ab420e78c2add28f5901dc
Raz <raz@jit.io>
2022-12-11 19:21:53 +0200

    Filter findings

--- pre_commit_hooks/vscode.py
+++ pre_commit_hooks/vscode.py
@@ -3,17 +3,24 @@ import os
 import hashlib
 
 
+def filter_findings(findings, configuration):
+    return list(filter(lambda finding: finding["controlName"] not in configuration), findings)
+
+
 def main():
     cwd = os.getcwd()
-    cwd_hash = hashlib.md5(cwd.encode('utf-8')).hexdigest()
-    with open('/tmp/controls/.precommit/' + cwd_hash + '.state.json', 'r') as json_file:
+    cwd_hash = hashlib.md5(cwd.encode("utf-8")).hexdigest()
+    with open("/tmp/controls/.precommit/" + cwd_hash + ".state.json", "r") as json_file:
         state = json.load(json_file)
+        findings = state.get("findings", [])
+        configuration = state.get("configuration", [])
+        if configuration:
+            findings = filter_findings(findings, configuration)
 
-    findings = state.get('findings', [])
     print(f"Found {len(findings)} findings")
     if findings:
         exit(1)
-    
+
     exit(0)
 
 
